8c7d05f0d0d81764907f5fa210d96c19a12fbd36
John Wesonga <johnwesonga@gmail.com>
2012-12-25 21:43:30 +0300

    added CRUD methods retrieve and delete

--- backend/mongo_backend.go
+++ backend/mongo_backend.go
@@ -1,6 +1,7 @@
 package backend
 
-import "labix.org/v2/mgo"
+import ("labix.org/v2/mgo"
+"labix.org/v2/mgo/bson")
 
 type MongoDBConn struct {
 	session *mgo.Session
@@ -31,6 +32,20 @@ func (m *MongoDBConn) Stop() {
 func (m *MongoDBConn) AddToDo(title, description string) (err error) {
 	c := m.session.DB("test").C("todo")
 	err = c.Insert(&ToDo{title, description})
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func (m *MongoDBConn) ListToDo(){
+  
+}
+
+func (m *MongoDBConn) DeleteToDo(id string) (err error) {
+	collection := m.session.DB("test").C("todo")
+	result := ToDo{}
+	err = collection.Find(bson.M{"_id": id})
 	if err != nil {
 		panic(err)
 	}
